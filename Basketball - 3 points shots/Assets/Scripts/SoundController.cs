eb72cbeaac12efe96de09382cf5150c4cbb0d8ad
shaharcalfon <shaharcalfon@gmail.com>
2020-08-31 23:04:57 +0300

    Create the arena

---
+++ Basketball - 3 points shots/Assets/Scripts/SoundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_BouncingBallAudio;
+    [SerializeField] private AudioSource m_BallOnRingAudio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            m_BouncingBallAudio.Play();
+        }
+        if(collision.collider.tag=="Ring")
+        {
+            m_BallOnRingAudio.Play();
+        }
+    }
+
+
+
+}
