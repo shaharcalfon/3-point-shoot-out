f487720105555b3bd3b547eb22ef23a4027ab0af
shaharcalfon <shaharcalfon@gmail.com>
2020-09-01 15:32:17 +0300

    update

--- Basketball - 3 points shots/Assets/Scripts/PlayerMovement.cs
+++ Basketball - 3 points shots/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector3 m_StartPosition = new Vector3(-8.79f, 0.67f, -16.41f);
+    
     private Transform m_PlayerTransform;
     private Transform m_Ball = null;
     [SerializeField] private Camera m_MainCamera;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,17 +17,19 @@ public class PlayerMovement : MonoBehaviour
         //Update the position of the camera to the start position.
         m_MainCamera.transform.position = m_StartPosition;
         m_PlayerTransform = transform;
-        transform.position = m_StartPosition;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(m_Ball!= null)
+        m_PlayerTransform.position += m_PlayerTransform.forward * (1f * Time.deltaTime);
+        if (m_Ball!= null)
         {
             m_Ball.transform.position = m_MainCamera.transform.position + m_MainCamera.transform.forward;
         }
         checkInput();
+        
     }
 
     void checkInput()
@@ -34,7 +38,9 @@ public class PlayerMovement : MonoBehaviour
         {
              
             RaycastHit hit;
+            Debug.Log(""+ Screen.width / 2f + "," + Screen.height / 2f);
             Ray myRay = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+            
             if (Physics.Raycast(myRay, out hit))
             {
                 if(hit.collider!= null)
