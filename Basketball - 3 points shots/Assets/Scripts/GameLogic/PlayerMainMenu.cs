5be0d33079e98015229ad78f15de27d2242b4c32
shaharcalfon <shaharcalfon@gmail.com>
2020-09-30 19:13:19 +0300

    Update

--- Basketball - 3 points shots/Assets/Scripts/GameLogic/PlayerMainMenu.cs
+++ Basketball - 3 points shots/Assets/Scripts/GameLogic/PlayerMainMenu.cs
@@ -6,8 +6,8 @@ public class PlayerMainMenu : MonoBehaviour
     [SerializeField] private Camera m_MainCamera;
     [SerializeField] private GameObject m_HowToPlayPopUp;
     [SerializeField] private GameObject m_HighScoresPopUp;
-    private float m_XScreenOffset = 0.1f;
-    private float m_YSceenOffset = 0.15f;
+    private float m_XScreenOffset = 0.257f;
+    private float m_YSceenOffset = 0.494f;
 
     void Update()
     {
@@ -20,9 +20,9 @@ public class PlayerMainMenu : MonoBehaviour
         {
             Debug.Log("Any Key Down");
             RaycastHit hit;
-            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3((Screen.width / 4f) - (m_XScreenOffset * Screen.width),
-                (Screen.height / 2f) - (m_YSceenOffset * Screen.height), 0f));
-            Debug.Log("Width: " + Screen.width / 4);
+            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3(m_XScreenOffset*Screen.width, m_YSceenOffset * Screen.height, 0f));
+            Debug.Log("Width: " + Screen.width);
+            Debug.Log("Height: " + Screen.height);
 
             if(Physics.Raycast(myRay, out hit))
             {
--- Basketball - 3 points shots/Assets/Scripts/GameLogic/playerController.cs
+++ Basketball - 3 points shots/Assets/Scripts/GameLogic/playerController.cs
@@ -5,7 +5,7 @@ public class playerController : MonoBehaviour
 {
     private const int NumberOfBallInRack = 4;
     private const int NumberOfBallsToThrow = 20;
-    [SerializeField] private float m_BallOffset = 0.93f;
+    [SerializeField] private float m_BallOffset = 0.96f;
     [SerializeField] private GameObject m_Player;
     [SerializeField] private Camera m_MainCamera;
 
@@ -17,13 +17,13 @@ public class playerController : MonoBehaviour
     [SerializeField] private Animator m_LeftHandAnimator;
     [SerializeField] private UIContoller m_UIController;
 
-    public Transform m_CurrentBall = null;
+    public Transform m_CurrentBall;
     private float m_BallThrowingForce = 515f;
     private float m_BallAngleForce = 180f;
     private Vector3[] throwPositions = new Vector3[5];
     private bool holdingBall = false;
-    private float m_XScreenOffset = 0.1f;
-    private float m_YSceenOffset = 0.065f;
+    private float m_XScreenOffset = 0.257f;
+    private float m_YSceenOffset = 0.494f;
 
     void Start()
     {
@@ -40,7 +40,7 @@ public class playerController : MonoBehaviour
         if(Input.anyKeyDown && !m_GameController.isGameOn)                      //Press the UI Buttons.
         {
             RaycastHit hit;
-            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3(Screen.width / 4f + m_XScreenOffset, Screen.height / 2f - m_YSceenOffset, 0f));
+            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3(m_XScreenOffset * Screen.width, m_YSceenOffset * Screen.height, 0f));
 
             if (Physics.Raycast(myRay, out hit))
             {
@@ -65,7 +65,7 @@ public class playerController : MonoBehaviour
         else if (Input.anyKeyDown && !holdingBall && m_GameController.isGameOn)   //The player try to catch ball
         {
             RaycastHit hit;
-            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3(Screen.width / 4f + m_XScreenOffset, Screen.height / 2f - m_YSceenOffset, 0f));
+            Ray myRay = m_MainCamera.ScreenPointToRay(new Vector3(m_XScreenOffset * Screen.width, m_YSceenOffset * Screen.height, 0f));
 
             if (Physics.Raycast(myRay, out hit))
             {
